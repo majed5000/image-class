37d9a56b165285a3a77d06b3c8ca031fb2d5ed16
majed5000 <66587310+majed5000@users.noreply.github.com>
2020-06-08 20:52:35 +0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/mgt5c6uphg6lwne/export.pkl?dl=0'
+export_file_url = 'https://www.dropbox.com/s/mgt5c6uphg6lwne/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 classes = ['orange', 'tangerine', 'lemon']
